0fa0368a8b4f1c03bcbf6d03f429963911f8bc4d
CPompey1 <Cpompey10@gmail.com>
2023-01-16 22:37:27 -0500

    Updated code

--- app/src/main/java/com/example/xplorecontroller/MainActivity.java
+++ app/src/main/java/com/example/xplorecontroller/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.xplorecontroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("Hello");
+
+        //Get Buttons
+
+        //Set Buttons onclick
+
+
     }
+
+    public void startConnection(){
+        try {
+            NetworkInterface ethInterface = NetworkInterface.getByName("eth0");
+            final InetAddress ip = (InetAddress) Collections.list(ethInterface.getInetAddresses()).get(1);
+        }
+
+        catch (IOException i){
+            print(i);
+        }
+
+        catch (SocketException i){
+
+        }
+    }
+
+    public void
+
+
 }
